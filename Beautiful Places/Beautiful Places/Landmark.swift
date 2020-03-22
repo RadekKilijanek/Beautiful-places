a51824dc788abcd7fc0fc3d353f04b218fad33db
Radek Kilijanek <62257237+RadekKilijanek@users.noreply.github.com>
2020-03-22 12:43:57 +0100

    Added value in landmark class

---
+++ Beautiful Places/Beautiful Places/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  Beautiful Places
+//
+//  Created by Radek Kilijanek on 22/03/2020.
+//  Copyright © 2020 Radek Kilijanek. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var Category: Category
+    
+    var locationCoordinate : CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case abandoned = "Abandoned"
+        case landscape = "Landscape"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
