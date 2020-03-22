7aec3a750dfd1ea857eb32201a54570d0f6bc2af
Radek Kilijanek <62257237+RadekKilijanek@users.noreply.github.com>
2020-03-22 12:06:57 +0100

    MapView class

---
+++ Beautiful Places/Beautiful Places/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Beautiful Places
+//
+//  Created by Radek Kilijanek on 22/03/2020.
+//  Copyright © 2020 Radek Kilijanek. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
