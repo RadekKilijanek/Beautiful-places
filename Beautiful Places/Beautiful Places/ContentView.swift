4017d0aeac41f2822310a2c98767d746c7636c00
Radek Kilijanek <>
2020-03-22 09:18:36 +0100

    New project in app

---
+++ Beautiful Places/Beautiful Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beautiful Places
+//
+//  Created by Radek Kilijanek on 22/03/2020.
+//  Copyright © 2020 Radek Kilijanek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
