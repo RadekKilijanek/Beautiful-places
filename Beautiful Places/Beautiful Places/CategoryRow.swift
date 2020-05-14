5f21057bd22d30b78785146371b4cb0f87661151
Radek Kilijanek <radek.kilijanek@gmail.com>
2020-05-14 12:39:40 +0200

    CategoryRow new struct CategoryItem

--- Beautiful Places/Beautiful Places/CategoryRow.swift
+++ Beautiful Places/Beautiful Places/CategoryRow.swift
@@ -13,8 +13,36 @@ struct CategoryRow: View {
     var items: [BeautifulPlace]
     
     var body: some View {
-        Text(self.categoryName)
-            .font(.headline)
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(self.items) { BeautifulPlace in
+                        CategoryItem(beautifulPlace: BeautifulPlace)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryItem: View {
+    var beautifulPlace: BeautifulPlace
+    var body: some View {
+        VStack(alignment: .leading) {
+            beautifulPlace.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(beautifulPlace.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
     }
 }
 
@@ -22,7 +50,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: beautifulPlaceData[0].category.rawValue,
-            items: Array( beautifulPlaceData.prefix(3) )
+            items: Array( beautifulPlaceData.prefix(4) )
         )
     }
 }
